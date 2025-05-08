d89a247c6b0d0a714e05e1558e63dc8978eb3eed
Joan Gomez <joangomezusma4@gmail.com>
2025-05-08 16:41:49 -0500

    Modificacion de base

--- src/test/java/com/SeleniumSchedulePro/base/BaseTest.java
+++ src/test/java/com/SeleniumSchedulePro/base/BaseTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.nio.file.Files;
 
 public class BaseTest {
     protected WebDriver driver;
@@ -12,11 +15,20 @@ public class BaseTest {
     //Prueba github act
 
     @BeforeEach
-    public void setUp() {
-        driver = new ChromeDriver();  // No necesitas setProperty
+    public void setUp() throws Exception {
+        ChromeOptions options = new ChromeOptions();
+
+        // Crea un directorio temporal para --user-data-dir
+        String tempProfileDir = Files.createTempDirectory("chrome-profile").toString();
+        options.addArguments("--user-data-dir=" + tempProfileDir);
+        options.addArguments("--headless=new"); // Para entornos sin GUI (como GitHub Actions)
+        options.addArguments("--disable-gpu");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
     }
-
     @AfterEach
     public void tearDown() {
         if (driver != null) {
